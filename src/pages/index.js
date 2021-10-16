84d563b24b4a58502afb324b9f6a03c271bcb05b
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-16 15:34:20 +0530

    2 files modified, 1 added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ import Explore from './Explore';
 import Home from './Home';
 import Notification from './Notification';
 import Profile from './Profile';
-export { Chat, Explore, Home, Notification, Profile };
+import Login from './Login';
+export { Chat, Explore, Home, Notification, Profile, Login };
